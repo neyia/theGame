accd78e1cdf6d30054af30f76458d10ae7aedeb6
Natallia <nzhyrkova@biarum.com>
2017-08-16 20:33:21 +0300

    added recovery pass page and success message

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,25 +1,49 @@
-
 (function ($) {
     $(function () {
         var $authPopup = $('#popup-auth');
         var $authBtn = $('#btn-auth');
         var $regPopup = $('#popup-registration');
+
         var $regBtn = $('#btn-registration');
         var $screen = $('.screen');
+
+        var $forgetBtn = $('#btn-forget');
+        var $screenInside = $('.screen--inside');
+        var $forgotPopup = $('#popup-forgot');
+
+        $forgetBtn.on('click', function (e) {
+            e.preventDefault();
+            var top = $(window).scrollTop();
+            $screenInside
+                .show()
+                .on('click', function (e) {
+                    e.preventDefault();
+                    $screenInside.hide();
+                    $forgotPopup.hide();
+                });
+            $forgotPopup.show().css('top', 100 + 'px');
+
+            $forgotPopup.find('.close-block__link')
+                .on('click', function (e) {
+                    e.preventDefault();
+                    $forgotPopup.hide();
+                    $screenInside.hide();
+                });
+        });
 
         $authBtn.on('click', function (e) {
             e.preventDefault();
             var top = $(window).scrollTop();
-                $screen
-                    .show()
-                    .on('click', function (e) {
-                        e.preventDefault();
-                        $screen.hide();
-                        $authPopup.hide();
-                    });
-                $authPopup.show().css('top',100+'px');
+            $screen
+                .show()
+                .on('click', function (e) {
+                    e.preventDefault();
+                    $screen.hide();
+                    $authPopup.hide();
+                });
+            $authPopup.show().css('top', 100 + 'px');
 
-                $authPopup.find('.close-block__link')
+            $authPopup.find('.close-block__link')
                 .on('click', function (e) {
                     e.preventDefault();
                     $authPopup.hide();
@@ -29,14 +53,14 @@
         $regBtn.on('click', function (e) {
             e.preventDefault();
             var top = $(window).scrollTop();
-                $screen
-                    .show()
-                    .on('click', function (e) {
-                        e.preventDefault();
-                        $screen.hide();
-                        $regPopup.hide();
-                    });
-            $regPopup.show().css('top', 10+'px');
+            $screen
+                .show()
+                .on('click', function (e) {
+                    e.preventDefault();
+                    $screen.hide();
+                    $regPopup.hide();
+                });
+            $regPopup.show().css('top', 10 + 'px');
 
             $regPopup.find('.close-block__link')
                 .on('click', function (e) {
